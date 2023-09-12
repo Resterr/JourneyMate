8252c05350e7ed21123cf96e71200f6abbe3d49f
Kamil Tomiak <52603787+Resterr@users.noreply.github.com>
2023-09-12 23:20:06 +0200

    Refactored Program.cs

---
+++ src/API/Extensions.cs
@@ -0,0 +1,36 @@
+﻿using JourneyMate.API.Middlewares;
+using JourneyMate.API.Services;
+using JourneyMate.Application.Common.Interfaces;
+using JourneyMate.Infrastructure.Persistence;
+
+namespace JourneyMate.API;
+public static class Extensions
+{
+	public static IServiceCollection AddPresentationLayer(this IServiceCollection services)
+	{
+		services.AddScoped<ErrorHandlingMiddleware>();
+		services.AddHttpContextAccessor();
+		services.AddScoped<ICurrentUserService, CurrentUserService>();
+		services.AddHealthChecks().AddDbContextCheck<ApplicationDbContext>();
+		services.AddControllers();
+		services.AddEndpointsApiExplorer();
+		services.AddSwaggerDocument();
+
+		return services;
+	}
+
+	public static WebApplication UsePresentationLayer(this WebApplication app)
+	{
+		app.UseHttpsRedirection();
+		app.UseMiddleware<ErrorHandlingMiddleware>();
+		app.UseHealthChecks("/health");	
+		app.UseOpenApi();
+		app.UseSwaggerUi3();
+		app.MapControllers();
+		app.UseAuthentication();
+		app.UseAuthorization();
+
+		return app;
+	}
+}
+
--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,9 +1,6 @@
-using JourneyMate.API.Middlewares;
-using JourneyMate.API.Services;
+using JourneyMate.API;
 using JourneyMate.Application;
-using JourneyMate.Application.Common.Interfaces;
 using JourneyMate.Infrastructure;
-using JourneyMate.Infrastructure.Persistence;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,30 +10,12 @@ builder.Host.UseSerilog((context, services, configuration) => configuration
 	.ReadFrom.Services(services)
 	.Enrich.FromLogContext());
 
-builder.Services.AddScoped<ErrorHandlingMiddleware>();
-
+builder.Services.AddPresentationLayer();
 builder.Services.AddApplicationLayer();
 builder.Services.AddInfrastructureLayer(builder.Configuration);
 
-builder.Services.AddHttpContextAccessor();
-builder.Services.AddScoped<ICurrentUserService, CurrentUserService>();
-builder.Services.AddHealthChecks().AddDbContextCheck<ApplicationDbContext>();
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerDocument();
-
 var app = builder.Build();
 
-app.UseMiddleware<ErrorHandlingMiddleware>();
-app.UseHealthChecks("/health");
-app.UseHttpsRedirection();
-
-app.UseOpenApi();
-app.UseSwaggerUi3();
-
-app.MapControllers();
-app.UseAuthentication();
-app.UseAuthorization();
+app.UsePresentationLayer();
 
 app.Run();
